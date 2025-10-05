20bf457b2a32d1219b480caf80b7e7a819accde3
Riya Rastogi <riya02rastogi@gmail.com>
2025-10-05 19:22:10 +0530

    about tool calling

--- app.js
+++ app.js
@@ -7,25 +7,23 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
   const completion = await groq.chat.completions.create({
-    temperature: 1,
+    temperature: 0,
     //top_p: 0.2, //either use this or temperature
     //stop: 'ga', // output will be Ne
     // max_completion_tokens: 1000,
     // max_tokens: '',
     // frequency_penalty:1,
     // presence_penalty:1,
-    response_format:{type: "json_object"},
+    //response_format:{type: "json_object"},
     model: "llama-3.3-70b-versatile",
     messages: [
         {
             role:"system",
-            content:`You are Dhriya, a smart review grader. Your task is to analyse given review and return sentiment. Classify the review as positive,neutral,negative. give the  output in json format
-            example : 
-            {"sentiment": "Positive"}` ,
+            content:"You smart personal assistant who answers the asked questions" ,
         },
       {
         role: "user",
-        content: "Review: These headphones arrived on time and look great, but the left earcup stopped working after a week.",
+        content: "When was iPhone 16 launched?",
       },
     ],
   });
