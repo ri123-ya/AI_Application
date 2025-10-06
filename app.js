21968f05fb464cd4e9ce053c83fc2ffffbcb58d5
Riya Rastogi <riya02rastogi@gmail.com>
2025-10-06 07:36:03 +0530

    getting web search ans with tool calling

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const tvly = tavily({ apiKey: process.env.TRAVILY_API_KEY });
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
+  const messages = [
+    {
+      role: "system",
+      content: `You smart personal assistant who answers the asked questions
+                   You have following tools:
+                   1. get_search({query} : {query: string}) //Search the correct  latest information from the web
+                   // But once the tool gives you the result, you must reply with a complete answer to the user.
+Never call the same tool again for the same query.`,
+    },
+    {
+      role: "user",
+      content: "When was iPhone 16 launched?",
+    },
+  ];
   const completion = await groq.chat.completions.create({
     temperature: 0,
     //top_p: 0.2, //either use this or temperature
@@ -18,18 +32,7 @@ async function main() {
     // presence_penalty:1,
     //response_format:{type: "json_object"},
     model: "llama-3.3-70b-versatile",
-    messages: [
-      {
-        role: "system",
-        content: `You smart personal assistant who answers the asked questions
-                   You have following tools:
-                   1. get_search({query} : {query: string}) //Search the correct  latest information from the web`,
-      },
-      {
-        role: "user",
-        content: "When was iPhone 16 launched?",
-      },
-    ],
+    messages: messages,
     tools: [
       {
         type: "function",
@@ -52,6 +55,7 @@ async function main() {
     tool_choice: "auto",
   });
 
+  messages.push(completion.choices[0].message);
   const toolCalls = completion.choices[0].message.tool_calls;
 
   if (!toolCalls) {
@@ -66,11 +70,44 @@ async function main() {
 
     if (functionName === "get_search") {
       const toolResult = await webSearch(JSON.parse(functionArgs));
-      console.log("Tool result: ", toolResult);
+      //console.log("Tool result: ", toolResult);
+
+      messages.push({
+        tool_call_id: tool.id,
+        role: "tool",
+        name: functionName,
+        content: toolResult,
+      });
     }
   }
 
-  //console.log(JSON.stringify(completion.choices[0].message.content,null,2));
+  const completion2 = await groq.chat.completions.create({
+    temperature: 0,
+    model: "llama-3.3-70b-versatile",
+    messages: messages,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "get_search",
+          description: "Search the correct  latest information from the web",
+          parameters: {
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The Search query to perform Search on",
+              },
+            },
+            required: ["query"],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto",
+  });
+
+  console.log(JSON.stringify(completion2.choices[0].message, null, 2));
 }
 main();
 
@@ -80,7 +117,11 @@ async function webSearch({ query }) {
   console.log("Searching the Web.....");
   const response = await tvly.search(query);
 
-  console.log("Response : ",response);
+  //console.log("Response : ", response);
 
-  return "Iphone was Launched on 17th May 2002";
+  const final = response.results.map((item) => item.content).join("\n\n");
+
+  //console.log("Final Answer : ", final);
+
+  return final;
 }
